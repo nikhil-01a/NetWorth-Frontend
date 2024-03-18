5fdc6aa322a5c7123f06c7609cf4b46c7b52c5d2
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-03-18 14:18:52 -0400

    updated files with 2 spaces for 2, Removed typescript lint rule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'networth-root',
-    standalone: true,
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'networth-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-    title = 'networth-Frontend';
+  title = 'networth-Frontend';
 }
